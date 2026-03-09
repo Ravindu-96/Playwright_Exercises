6bf47dff3472966b04f67898f0d82c1102c0fd81
Ravindu-96 <nethmina.kar@gmail.com>
2026-03-09 14:37:00 +0530

    group login flow

--- pageobjects/login-page.js
+++ pageobjects/login-page.js
@@ -1,20 +1,18 @@
 class LoginPage {
     constructor(page) {
+        this.url = "https://www.saucedemo.com/";
         this.page = page;
         this.usernameInput = page.locator('#user-name');
         this.passwordInput = page.locator('#password');
         this.loginButton = page.locator('.submit-button');
     }
-    async gotoPage(url) {
-        await this.page.goto("https://www.saucedemo.com/");
+    async gotoPage() {
+        await this.page.goto(this.url);
     }
     async login(username, password) {
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
         await this.loginButton.click();
-    }
-    async getPageTitle() {
-        await this.page.goto('https://www.saucedemo.com/')
     }
 }
 export { LoginPage };
